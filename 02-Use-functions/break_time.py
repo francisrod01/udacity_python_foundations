5d495eb403c6125bcc854c4cf69c644b2fafb5b2
Francis Rodrigues <dev@tosystems.net>
2017-06-11 14:40:27 -0300

    Improved while loop on break time script

--- 02-Use-functions/break_time.py
+++ 02-Use-functions/break_time.py
@@ -3,10 +3,13 @@
 import time
 import webbrowser
 
-count = 0
-while count < 3:
+total_breaks = 3
+break_count = 0
+
+print("This program started on " + time.ctime())
+while break_count < total_breaks:
     time.sleep(10)
     webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    count += 1
+    break_count += 1
 
 print("Good bye!")
