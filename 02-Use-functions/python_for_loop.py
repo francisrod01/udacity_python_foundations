0555d16339c4e98c63546b18ea51136b6b3c953a
Francis Rodrigues <dev@tosystems.net>
2017-06-11 19:38:54 -0300

    Implementing for loops

---
+++ 02-Use-functions/python_for_loop.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+print('# First example')
+for letter in 'Python':
+    print('Current letter: ', letter)
+
+print('\n# Second example')
+fruits = ['banana', 'apple', 'mango']
+for fruit in fruits:
+    print('Current fruit: ', fruit)
+
+print('\n# Using else statement with for loops')
+for num in range(10, 20):  # to iterate between 10 to 20
+    for i in range(2, num):  # to iterate on the factors of the number
+        if num % i == 0:  # to determine the first factor
+            j = num / i  # to calculate the second factor
+            print('%d equals %d * %d' % (num, i, j))
+            break  # to move to the next number, the first FOR
+    else:
+        print(num, 'is a prime number')
+
+print('\nGood bye!')
