e1ce8ca481c418243659ac3bb1dc360e0b222205
Francis Rodrigues <dev@tosystems.net>
2017-06-11 20:36:11 -0300

    Improved renaming a files and squashing a bug

--- 02-Use-functions/21-Opening_a_file/rename_files.py
+++ 02-Use-functions/21-Opening_a_file/rename_files.py
@@ -5,11 +5,15 @@ import os
 def rename_files():
     # (1) get file names from a folder
     file_list = os.listdir("./prank")
-    print(file_list)
+    saved_path = os.getcwd()
+    print('Current working directory is ' + saved_path)
+    os.chdir(saved_path + '/prank')
 
     # (2) for each file, rename filename
     for file_name in file_list:
         str = file_name.maketrans('', '', '0123456789')
+        print('Old Name: ' + file_name, '- New Name: ' + file_name.translate(str))
         os.rename(file_name, file_name.translate(str))
+    os.chdir(saved_path)
 
 rename_files()
