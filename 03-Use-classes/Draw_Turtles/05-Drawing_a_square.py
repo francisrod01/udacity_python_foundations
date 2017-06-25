3df66dff6eec5cbace9345505d59d2202531ac50
Francis Rodrigues <dev@tosystems.net>
2017-06-25 19:37:26 -0300

    Changing turtle's shape, color, speed, background

--- 03-Use-classes/Draw_Turtles/05-Drawing_a_square.py
+++ 03-Use-classes/Draw_Turtles/05-Drawing_a_square.py
@@ -5,9 +5,19 @@ import turtle
 
 def draw_square():
     window = turtle.Screen()
-    window.bgcolor("red")
+    window.bgcolor("black")
 
     bad = turtle.Turtle()
+    bad.shape("turtle")
+    bad.speed(3)
+    bad.color("yellow")
+
+    bad.forward(100)
+    bad.right(90)
+    bad.forward(100)
+    bad.right(90)
+    bad.forward(100)
+    bad.right(90)
     bad.forward(100)
 
     window.exitonclick()
