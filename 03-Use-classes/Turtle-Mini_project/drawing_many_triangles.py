fcb04307d315409bedb0e391a4e5bd884b7752b6
Francis Rodrigues <dev@tosystems.net>
2017-07-09 19:25:35 -0300

    ENH: Improved triangle creation

--- 03-Use-classes/Turtle-Mini_project/drawing_many_triangles.py
+++ 03-Use-classes/Turtle-Mini_project/drawing_many_triangles.py
@@ -1,9 +1,12 @@
 #!~/envs/udacity-python-env
 
+# A contribution of Thai Thien to the Udacity discussion forum
+# https://discussions.udacity.com/t/turtle-mini-project-f/16121/8?u=francisrod01
+
 import turtle
 
 
-def draw_triangle(the_turtle, length, recursion):
+def draw_triangle(the_turtle, length, origin, recursion):
     recursion += 1
     brad = the_turtle
 
@@ -11,7 +14,16 @@ def draw_triangle(the_turtle, length, recursion):
         if recursion < 4:
             brad.forward(length / 2)
             brad.left(120)
-            draw_triangle(brad, length / 2, recursion)
+            draw_triangle(brad, length / 2, 0, recursion)
+            brad.right(120)
+            brad.forward(length / 2)
+        else:
+            brad.forward(length)
+
+        if origin == 1:
+            brad.left(120)
+        else:
+            brad.right(120)
 
 
 def draw_art():
@@ -24,7 +36,7 @@ def draw_art():
     background = turtle.Screen()
     background.bgcolor("grey")
 
-    draw_triangle(brad, 200, 0)
+    draw_triangle(brad, 200, 1, 0)
 
     background.exitonclick()
 
