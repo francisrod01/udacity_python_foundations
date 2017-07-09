4a8573975523b5bda77e8bf8840d22ba1c6dcfc1
Francis Rodrigues <dev@tosystems.net>
2017-07-09 18:41:31 -0300

    Turtle - Mini project -- Drawing a flower

---
+++ 03-Use-classes/Turtle-Mini_project/drawing_a_flower.py
@@ -0,0 +1,39 @@
+#!~/envs/udacity-python-env
+
+import turtle
+
+
+def draw_flower(some_turtle):
+    for i in range(1, 3):
+        some_turtle.forward(100)
+        some_turtle.right(60)
+        some_turtle.forward(100)
+        some_turtle.right(120)
+
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("grey")
+
+    # Create the turtle Brad - Draws a square
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.speed(20)
+
+    brad.setheading(270)
+    brad.color("green")
+    brad.pensize(10)
+    brad.forward(400)
+    brad.pensize(4)
+    brad.home()
+    brad.color("yellow")
+
+    # Put draw of square in loop to draw a flower
+    for i in range(0, 36):
+        draw_flower(brad)
+        brad.right(10)
+
+    window.exitonclick()
+
+
+draw_art()
