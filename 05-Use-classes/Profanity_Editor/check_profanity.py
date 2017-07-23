ab5bee5c466b7deed076af71660775262bbb7d50
Francis Rodrigues <dev@tosystems.net>
2017-07-23 12:21:33 -0300

    ENH: Implemented profanity verification script - part 02

--- 05-Use-classes/Profanity_Editor/check_profanity.py
+++ 05-Use-classes/Profanity_Editor/check_profanity.py
@@ -1,10 +1,20 @@
 #!~/envs/udacity-python-env
 
+import urllib.request as request
+import urllib.parse as parse
+
 def read_text():
     quotes = open("./files/movie_quotes.txt")
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
+    check_profanity(contents_of_file)
 
+def check_profanity(text_to_check):
+    url = "http://www.wdylike.appspot.com/?q=" + parse.quote(text_to_check)
+    connection = request.urlopen(url)
+    output = connection.read()
+    print(output)
+    connection.close()
 
 read_text()
--- README.md
+++ README.md
@@ -11,6 +11,8 @@ See author notes:
 - Beginning with Python at [Loyola University](http://anh.cs.luc.edu/python/hands-on/3.1/handsonHtml/ch1.html)
 - Tutorials at [TutorialsPoint](https://www.tutorialspoint.com/python/index.htm)
 - Using Python3 in [virtualenv](https://stackoverflow.com/a/23842752/3332734)
+- Python Doc - [Built-in functions](https://docs.python.org/3.6/library/functions.html)
+- Checking for [Curse Words](http://www.wdylike.appspot.com/?q=shot)
 
 ## Packages ##
 
