83129cd108530967830989a7dce8c527fd9d523a
Francis Rodrigues <dev@tosystems.net>
2017-07-23 15:15:57 -0300

    Defined __init__ to class Movie

---
+++ 06-Make-classes/Movie_Website/media.py
@@ -0,0 +1,8 @@
+#!~/envs/udacity-python-env
+
+class Movie:
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image = poster_image
+        self.trailer_youtube_url = trailer_youtube
