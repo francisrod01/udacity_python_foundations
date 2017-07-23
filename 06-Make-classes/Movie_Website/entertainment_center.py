e5e505ecc15cd943d36d6f2d7c22588df4907f43
Francis Rodrigues <dev@tosystems.net>
2017-07-23 15:25:18 -0300

    Create instance to Avatar

--- 06-Make-classes/Movie_Website/entertainment_center.py
+++ 06-Make-classes/Movie_Website/entertainment_center.py
@@ -8,4 +8,12 @@ poster_image = "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg"
 trailer_youtube = "https://www.youtube.com/watch?v=vwyZH85NQC4"
 
 toy_story = media.Movie(title, movie_storyline, poster_image, trailer_youtube)
-print(toy_story.storyline)
+# print(toy_story.storyline)
+
+title = "Avatar"
+movie_storyline = "A marine on an alien planet"
+poster_image = "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg"
+trailer_youtube = "https://www.youtube.com/watch?v=-9ceBgWV8io"
+
+avatar = media.Movie(title, movie_storyline, poster_image, trailer_youtube)
+print(avatar.storyline)
